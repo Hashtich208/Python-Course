9037f22778791f967efb532d2b34ab33061ae04a
Hashtich208 <44102225+Hashtich208@users.noreply.github.com>
2018-10-19 01:38:04 +0300

    Add files via upload

---
+++ Week 1/Practice1-A.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+user_input = input("Enter your name: " )
+First_Number = input("Enter the First Number: ")
+Second_Number = input("Enter the Second Number: ")
+print(f"Hello, {user_input} ") 
+print(f"The sum of {First_Number} and {Second_Number} is: ", end="")
+print(int(First_Number) + int(Second_Number))
+
+print(f"The Sub of {First_Number} and {Second_Number} is: ", end="")
+print(int(First_Number) - int(Second_Number))
+
+print(f"The Mul of {First_Number} and {Second_Number} is: ", end="")
+print(int(First_Number) * int(Second_Number))
+
+print(f"The Div of {First_Number} and {Second_Number} is: ", end="")
+print(int(First_Number) / int(Second_Number))
+
+print(f"The Reminder of {First_Number} over {Second_Number} is: ", end="")
+print(int(First_Number) % int(Second_Number))
+
+print(f"The Exponent of {First_Number} to the power of {Second_Number} is: ", end="")
+print(int(First_Number) ** int(Second_Number))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
